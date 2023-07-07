e6ea74cfce6634d2adefc6a7be9dae0bc746bce1
Alenios23 <135436183+Alenios23@users.noreply.github.com>
2023-07-07 12:09:00 +0200

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello : MonoBehaviour
+{
+    // prikaz prvog printa, na pocetku skripte
+    void Start()
+    {
+        print("zdravo") ; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("petak") ;
+    }
+}
